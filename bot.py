3fcf191c2c1f5aef9e463bb185a89936fe449fa7
Koral <62809012+KoralK5@users.noreply.github.com>
2021-07-06 08:15:37 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ from PIL import Image, ImageGrab
 from win32api import GetSystemMetrics
 from keyboard import press_and_release
 
-x, y = GetSystemMetrics(0),GetSystemMetrics(1)
+x, y = GetSystemMetrics(0), GetSystemMetrics(1)
 while True:
 	img = np.array(ImageGrab.grab(bbox=(x//2, y-171, x//2+2, y-170)))
 	color = Image.fromarray(img).convert('RGB').getcolors()
